3c55ac5ac8169e9519a7d10de9fd43bdbcca5ac3
Dennis Vu <dennisvu65@gmail.com>
2021-03-05 15:26:50 -0800

    fix bug: - Having infinite loop problem in one of the codes.

--- investment.py
+++ investment.py
@@ -4,16 +4,13 @@ def calculate_apr(principal, interest_rate, years):
     """
     i=0
     output = 0
-    while i != years:
-        if i == years:
-            output = principal*(1 + interest_rate)**(years)
+    for i in range(years):
+        output = principal*(1 + interest_rate)**(years)
+        if principal < 0:
+            return(bool(0))
+        elif interest_rate < 0:
+            return(bool(0))
         else:
-            i = i + 1
-    if principal < 0:
-        return(bool(0))
-    elif interest_rate < 0:
-        return(bool(0))
-    else:
-        return(bool(0))
+            return(bool(0))
     print(output)
 
