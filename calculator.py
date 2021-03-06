ec4dbdd4e68a6600a2490832cf8028c7af2f7ebd
Dennis Vu <dennisvu65@gmail.com>
2021-03-05 21:14:02 -0800

    docs: - Final documentation and comments for all 3 files: * investment.py * calculator.py * timing.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 def calculator(number1, number2, operator):    
     """
-    Calculator uses the following functions:
+    Calculator uses the following operators on numbers 1 and 2:
     
     addition (+),
     subtraction(-),
@@ -12,10 +12,10 @@ def calculator(number1, number2, operator):
     If the operator is invalid, the program exits
     """
     output = 0
-    if operator == '+':
+    if operator == '+': 
         output = number1 + number2
-        return float(output)
-    elif operator == '-':
+        return float(output)  
+    elif operator= '-':
         output = number1 - number2
         return float(output)
     elif operator == '*':
@@ -31,21 +31,27 @@ def calculator(number1, number2, operator):
         output = number1 ** number2
         return float(output)
     else: 
-        return False
+        return False #this condition makes it so that anything other than a string passes 
     
 
 
 	
-def input_output(x):
+def input_output():
     '''
     Asks if the user wishes to continue, if not, the program exits
     '''
-    print('do you wish to exit?')
-    if x == 'y':
-        return
-    elif x == 'n':
-        calculator(number1,number2,operation)
-    else:
-        print('do you wish to exit?')
+    i = 0 
+    while i == 0: #this statement is for an infinite loop 
+        
+        input1 = input('Enter the first number: ')
+        input2= input('enter the second number: ')
+        operation = input('enter the operator: ')
+        
+        exit = ('Do you wish to exit? ')
+        
+        if exit == y
+             break  #breaks the loop
+        
+        
 
                                
--- investment.py
+++ investment.py
@@ -2,10 +2,10 @@ def calculate_apr(principal, interest_rate, years):
     """
     Calculates the interest rate with years
     """
-    i= -1
+    i= -1 #for the in range method I offset it by 1 to match the years value.
     output = 0
     
-    if principal <= 0:
+    if principal <= 0: #I did these if statements to check all 3 parameters.
         return False
     elif interest_rate <= 0:
         return False
@@ -15,5 +15,5 @@ def calculate_apr(principal, interest_rate, years):
         for i in range(years):
             i = i + 1    
             output = principal*(1 + interest_rate)**years
-            return float(output)
-    
+            return float(output) 
+   
